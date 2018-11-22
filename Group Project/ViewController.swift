1efa91a88775e6fcbee8a9216aa46b183d4038bd
Samuel Lawes (s5061657) <s5061657@mac42933.student.bournemouth.ac.uk>
2018-11-22 11:45:56 +0000

    Initial Commit

---
+++ Group Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Group Project
+//
+//  Created by Samuel Lawes (s5061657) on 22/11/2018.
+//  Copyright © 2018 Samuel Lawes (s5061657). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
