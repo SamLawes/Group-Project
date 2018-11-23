ed6c1999a1701a35352b32c05419070738cf0898
Sam Lawes <samlawes99@gmail.com>
2018-11-23 15:15:09 +0000

    AppManager Created

---
+++ Group Project/AppManager.swift
@@ -0,0 +1,9 @@
+import Foundation
+import UIKit
+import Firebase
+
+class AppManager {
+    static let shared = AppManager()
+    private let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    private init() {}
+}
