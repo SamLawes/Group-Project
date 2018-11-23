1fa0f91493ad802f3cde8bce282a34c1b3d346a0
Harry Kingston (s5104866) <s5104866@mac38572.student.bournemouth.ac.uk>
2018-11-23 15:52:19 +0000

    yeet with mapkit in right place

--- Group Project/ViewController.swift
+++ Group Project/ViewController.swift
@@ -1,11 +1,4 @@
-//
-//  ViewController.swift
-//  Group Project
-//
-//  Created by Samuel Lawes (s5061657) on 22/11/2018.
-//  Copyright © 2018 Samuel Lawes (s5061657). All rights reserved.
-//
-
+import mapkit
 import UIKit
 
 class ViewController: UIViewController {
