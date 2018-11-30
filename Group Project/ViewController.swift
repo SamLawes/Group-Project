4c88c62d11bca6ce7baa3847e3b1ead500755efa
Harry Kingston <harrykingston182@gmail.com>
2018-11-30 15:45:04 +0000

    Accelerometer

--- Group Project/ViewController.swift
+++ Group Project/ViewController.swift
@@ -1,13 +1,27 @@
 //import mapkit
 import UIKit
-
+import CoreMotion
 class ViewController: UIViewController {
-
+    
+    var motionMagnager = CMMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+override func viewDidAppear(_ animated: Bool)
+{
+    motionMagnager.accelerometerUpdateInterval = 0.2
+    
+    motionMagnager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
+        if let myData = data
+        {
+            if myData.acceleration.x == 5 {
+    }
+        print("Run started")
 }
 
+}
+}
+}
