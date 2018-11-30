72bcd379cb445617c79dd0e52a41fb9b24d3a788
Jack Lawrence (s5070751) <s5070751@mac38567.student.bournemouth.ac.uk>
2018-11-30 14:46:16 +0000

    login screen update1

---
+++ Group Project/AppManagerContainer.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+class AppManagerContainer: UIViewController {
+    
+    override func viewDidAppear(_ animated: Bool){
+        super.viewDidAppear(animated)
+        
+        AppManager.shared.appContainer = self
+        AppManager.shared.showApp()
+    
+    
+    
+}
