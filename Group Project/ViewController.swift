25a61cb2980b4162d58e10ee14a72fa100a81b87
Calum Kirkland (s5110542) <s5110542@mac38571.student.bournemouth.ac.uk>
2018-11-30 14:10:06 +0000

    Think this is the Marker

--- Group Project/ViewController.swift
+++ Group Project/ViewController.swift
@@ -15,6 +15,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+  //  func GetRoughtofTwoLocation(){
+      //  let originString: String = "\(23.5800),\(72.5853)"
+      //  let destinationString: String = "\(24.5836),\(72.5853)"
+      //  let directionsAPI: String = "https://maps.googleapis.com/maps/api/directions/json?"
+//let directionsUrlString: String = "\(directionsAPI)&origin=\(originString)&destination=\(destinationString)&mode=driving"
+        
+        //APICall().callApiUsingWithFixURLGET(directionsUrlString, withLoader: true) { (responceData) -> Void in
+         //   let json = responceData as! NSDictionary
+         //   let routesArray: [AnyObject] = (json["routes"] as! [AnyObject])
+         //   var polyline: GMSPolyline? = nil
+         //   if routesArray.count > 0 {
+             //   let routeDict: [NSObject : AnyObject] = routesArray[0] as! [NSObject : AnyObject]
+               // var routeOverviewPolyline: [NSObject : AnyObject] = (routeDict["overview_polyline"] as! [NSObject : AnyObject])
+               // let points: String = (routeOverviewPolyline["points"] as! String)
+              //  let path: GMSPath = GMSPath(fromEncodedPath: points)!
+              //  polyline = GMSPolyline(path: path)
+              //  polyline!.strokeWidth = 2.0
+              //  polyline!.map = self.mapView
+          //  }
+            
+      //  }
+   // }
 }
 
