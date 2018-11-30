e476fb8883c2f8750fc8eb9ac8b98269e8cf8134
Calum Kirkland (s5110542) <s5110542@mac38571.student.bournemouth.ac.uk>
2018-11-30 15:51:21 +0000

    Marker

--- Group Project/ViewController.swift
+++ Group Project/ViewController.swift
@@ -1,13 +1,14 @@
 //import mapkit
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    let location = CLLocationCoordinate2D(latitude: 50.716714, longitude: -1.875833)
+    let region = MKCoordinateRegion
 }
-
