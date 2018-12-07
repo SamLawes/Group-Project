3f14d540fcdc9fd5f7a3d606927f7dd36ab1db76
Harry Kingston <harrykingston182@gmail.com>
2018-12-07 14:16:15 +0000

    Working login

--- Group Project/Loginviewcontroller.swift
+++ Group Project/Loginviewcontroller.swift
@@ -1,17 +1,20 @@
-//
-//  Loginviewcontroller.swift
-//  Group Project
-//
-//  Created by Jack Lawrence (s5070751) on 30/11/2018.
-//  Copyright © 2018 Samuel Lawes (s5061657). All rights reserved.
-//
-
 import Foundation
 import UIKit
 import Firebase
 
 
+
+
 class LoginViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        emailTextField.text = "test@test.com"
+        password.text = "password"
+        
+        
+    }
     
     @IBOutlet weak var emailTextField: UITextField!
     
