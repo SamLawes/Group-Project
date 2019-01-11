adb9391c2b24520f4fe3c020b29b27dff975b612
Harry Kingston <harrykingston182@gmail.com>
2019-01-11 15:54:08 +0000

    Table fix

--- Group Project/MenuViewController.swift
+++ Group Project/MenuViewController.swift
@@ -3,41 +3,41 @@ import Foundation
 import UIKit
 import Firebase
 
-class MenuViewController: UIViewController, UITableViewDelegate {
-    
-    
-    @IBOutlet weak var MenuTable: UITableView!
+class MenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
     
     var runs = [Run]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-//        tableView.separatorColor = UIColor(white: 0.95, alpha: 1)
-//        tableView.delegate = self
-       loadRuns()
-    }
-    
-    func loadRuns() {
+        tableView.delegate = self
         
         let ref = Firestore.firestore().collection("users").document(Auth.auth().currentUser!.uid).collection("runs")
         ref.getDocuments { snapshot, error in
+            self.runs.removeAll()
             for document in snapshot!.documents {
                 let run = Run(document: document)
                 self.runs.append(run)
-                self.MenuTable.reloadData()
             }
+            
+//            self.runs.sort(by: { (Run) -> Bool in
+//            })
+            self.tableView.reloadData()
+
+    
         }
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.Run = self.runs[indexPath.row]
+    }
     
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection Section:Int ) -> Int {
-//        return 8
-//    }
-//
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        return 1
-//    }
-//    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection Section:Int ) -> Int {
+        return runs.count
+    }
+    
     
 }
