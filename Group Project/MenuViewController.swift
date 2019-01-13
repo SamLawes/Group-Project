f6d68bfbf1eb09ac4aea7d45f4c9de4b775701ab
Sam Lawes <samlawes99@gmail.com>
2019-01-13 16:43:20 +0000

    Commented Code

--- Group Project/MenuViewController.swift
+++ Group Project/MenuViewController.swift
@@ -3,8 +3,8 @@ import Foundation
 import UIKit
 import Firebase
 
-class MenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+class MenuViewController: UIViewController, UITableViewDelegate {
+//, UITableViewDelegate, UITableViewDataSource
     @IBOutlet weak var tableView: UITableView!
     
     var runs = [Run]()
@@ -13,31 +13,32 @@ class MenuViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidLoad()
         tableView.delegate = self
         
-        let ref = Firestore.firestore().collection("users").document(Auth.auth().currentUser!.uid).collection("runs")
-        ref.getDocuments { snapshot, error in
-            self.runs.removeAll()
-            for document in snapshot!.documents {
-                let run = Run(document: document)
-                self.runs.append(run)
-            }
-            
-//            self.runs.sort(by: { (Run) -> Bool in
-//            })
-            self.tableView.reloadData()
+//        let ref = Firestore.firestore().collection("users").document(Auth.auth().currentUser!.uid).collection("runs")
+//        ref.getDocuments { snapshot, error in
+//            self.runs.removeAll()
+//            for document in snapshot!.documents {
+//                let run = Run(document: document)
+//                self.runs.append(run)
+//            }
+//
+//            //self.runs.sort(by: { (Run) -> Bool in
+//            //})
+//            self.tableView.reloadData()
+//
+//
+//        }
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+//
+//        cell.Run = self.runs[indexPath.row]
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection Section:Int ) -> Int {
+//        return runs.count
+//    }
 
-    
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
-        cell.Run = self.runs[indexPath.row]
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection Section:Int ) -> Int {
-        return runs.count
-    }
-    
-    
+
+}
 }
